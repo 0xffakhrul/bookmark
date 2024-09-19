546aacc4f68363ec5ba1f5e46deb84bd59529fc9
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-09-19 20:52:58 +0800

    add toast

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,63 @@
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import Layout from "./components/Layout";
 import { Signup } from "./pages/auth/Signup";
 import { Login } from "./pages/auth/Login";
-import { Dashboard } from "./pages/Dashboard";
 import { useCurrentUser } from "./api/user";
 import { Blank } from "./pages/Blank";
+import Dashboard from "./pages/Dashboard";
+import { New } from "./pages/New";
 
 function App() {
-  const { data: currentUser } = useCurrentUser();
+  const { data: currentUser, isLoading, isError } = useCurrentUser();
+  const location = useLocation();
 
-  console.log(currentUser);
+  useEffect(() => {
+    console.log('Current path:', location.pathname);
+    console.log('Current user:', currentUser);
+    console.log('Is loading:', isLoading);
+    console.log('Is error:', isError);
+  }, [location, currentUser, isLoading, isError]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Blank />} />
-        <Route path="/signup" element={ <Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={currentUser ? <Navigate to="/dashboard" /> : <Blank />}
+        />
+        <Route
+          path="/signup"
+          element={currentUser ? <Navigate to="/dashboard" /> : <Signup />}
+        />
+        <Route
+          path="/login"
+          element={currentUser ? <Navigate to="/dashboard" /> : <Login />}
+        />
+        <Route
+          path="/dashboard"
+          element={
+            currentUser ? (
+              <Dashboard currentUser={currentUser} />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+        <Route
+          path="/new"
+          element={
+            currentUser ? (
+              <New />
+            ) : (
+              <Navigate to="/login" state={{ from: location }} replace />
+            )
+          }
+        />
       </Routes>
     </Layout>
   );
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
+        <Toaster />
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
