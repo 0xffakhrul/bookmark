dad10a7913a16194afc559ccc971f9d7c3628be1
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2024-09-19 08:45:10 +0800

    fix cors to handle credentials

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import auth from "./routes/auth";
+import bookmarks from "./routes/bookmarks";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.listen(port, () => {
@@ -29,3 +38,4 @@ const connectDB = async () => {
 connectDB();
 
 app.use("/api/auth", auth);
+app.use("/api/bookmarks", bookmarks);
